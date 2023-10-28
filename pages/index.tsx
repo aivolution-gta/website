6ba3f893247be2796389263dc3b3ef2c96d085f7
Rohan <114675334+rdnm0@users.noreply.github.com>
2023-10-28 00:12:25 -0400

    updated aivolution text opacity (makes it more clear)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home () {
     return (
         <div className="w-full">
-                <h1 className="flex justify-center place-center my-20 opacity-75">AiVolution</h1>
+                <h1 className="flex justify-center place-center my-20 opacity-100">AiVolution</h1>
         </div>
     )
 }
