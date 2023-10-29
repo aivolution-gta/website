556fa6d056eef5d5ddef4e905d1914f569ab43ba
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-10-29 16:30:18 -0400

    Separated the headings

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,32 @@
 export default function About () {
     return (
         <div className="w-screen">
-            <div className="w-full">
-                <h1 className="flex justify-center place-center my-20 opacity-75">About Us</h1>
+            {/* Section 1 */}
+            <div className="bg-red-100">
+                <div className="w-full">
+                    <h1 className="flex justify-center place-center my-20 opacity-75">About Us</h1>
+                </div>
+                <div className="w-full">
+                    <h3 className="flex text-center justify-center place-center my-20 opacity-75 text-3xl mx-36">
+                        A non-profit organization focused around informing people about our AI driven future.
+                    </h3>
+                </div>
+                <div>
+                    <p className="flex text-center justify-center place-center my-20 opacity-75 mx-96">
+                        At AiVolution, we are passionate about the infinite possibilities that technology offers and are dedicated to fostering a community that shares this enthusiasm. We are a non-profit organization, and our mission is clear: to be at the forefront of the ever-evolving world of technology by bringing people together through a series of exceptional events and conferences.
+                    </p>
+                </div>
             </div>
-            <div className="w-full">
-                <h3 className="flex text-center justify-center place-center my-20 opacity-75 text-3xl mx-36">
-                    A non-profit organization focused around informing people about our AI driven future.
-                </h3>   
+
+            {/* Section 2 */}
+            <div>
+                <div>
+                    <div className="w-full">
+                        <h1 className="flex justify-center place-center my-20 opacity-75">Who Are We</h1>
+                    </div>
+                </div>
             </div>
+            
         </div>  
     )   
 }
