d4d2225e5163f46b9b7e62a5d5653081c4cdb74d
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-10-28 21:50:58 -0400

    Initial commit

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,21 @@
 export default function About () {
     return (
-        <div className="w-full">
+        <div>
+            <div className="w-full">
                 <h1 className="flex justify-center place-center my-20 opacity-75">About</h1>
+            </div>
+            <AboutInfo />
+        </div>  
+    )   
+}
+
+function AboutInfo() {
+    return (
+        <div className="w-full">
+            <h2 className="flex justify-left place-center my-20 font-sans font-normal">What is AiVolution</h2>
+            <p> AiVolution</p>
+            <h2 className="flex justify-left place-center my-20 font-sans font-normal">Who We Are</h2>
         </div>
     )
 }
+
