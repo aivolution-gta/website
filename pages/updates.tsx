a7514310e9bb9de31d8c0c526d95d9bf25ba05c8
Advay Chandorkar <advay1612@icloud.com>
2023-10-30 20:04:19 -0400

    responsive countdown till next summit

--- pages/updates.tsx
+++ pages/updates.tsx
@@ -1,7 +1,12 @@
-export default function Updates () {
-    return (
-        <div className="w-full">
-                <h1 className="flex justify-center place-center my-20 opacity-75">Updates</h1>
-        </div>
-    )
+import React from "react";
+import ShiftingCountdown from "../components/countdown";
+
+export default function Updates() {
+  return (
+    <div className="w-full">
+      <h1 className="flex justify-center place-center my-5">Updates</h1>
+      <h2 className="flex justify-center place-center">Days Till Next Summit</h2>
+      <ShiftingCountdown /> 
+    </div>
+  );
 }
