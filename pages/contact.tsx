801b5985cd2edcc745480a97fb99e1fb4040295a
rdnm0 <rohandhanam@gmail.com>
2023-12-04 18:54:27 -0500

    added a quick placeholder footer element

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -28,9 +28,15 @@ export default function Contact() {
         </div>
       </div>
 
+      
+      <footer className="bg-gray-200 py-4 text-center">
+        <p className="text-gray-600">Contact us at "Aivolutionemailplaceholder"</p>
+      </footer>
+    </div>
+  );
+}
+
 
       
       
-    </div>
-  );
-  }
+   
