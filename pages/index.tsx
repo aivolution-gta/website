4f67525dc4473bb26b143f60f07f877a9fdad6f3
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-12-04 20:28:40 -0500

    Formatted code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,32 +6,32 @@ import Team from "@/components/team/team";
 export default function Home () {
     return (
         <div className="w-full">
+            {/* Section 1 */}
             <h1 className="flex justify-center place-center my-20 opacity-100">AiVolution</h1>
-            <div className="w-screen">
-                {/* Section 1 */}
-                <div className="py-20">
-                    <div className="w-full">
-                        <h1 className="flex justify-center place-center my-20 opacity-75 my-0 ">About Us</h1>
-                    </div>
-                    <div className="w-full">
-                        <h3 className="flex text-center justify-center place-center my-20 opacity-75 text-3xl mx-36 ">
-                            A non-profit organization focused around informing people about our AI driven future.
-                        </h3>
-                    </div>
-                    <div>
-                        <p className="flex text-center justify-center place-center my-20 opacity-75 mx-96 my-0">
-                            At AiVolution, we are passionate about the infinite possibilities that technology offers and are dedicated to fostering a community that shares this enthusiasm. We are a non-profit organization, and our mission is clear: to be at the forefront of the ever-evolving world of technology by bringing people together through a series of exceptional events and conferences.
-                        </p>
-                    </div>
-                </div>
 
-                {/* Section 2 */}
-                <div className="py-20">
-                    <div className="w-full">
-                        <h1 className="flex justify-center place-center my-20 opacity-75 my-0">Who Are We</h1>
-                    </div>
-                    <Team />
+            {/* Section 2 */}
+            <div className="py-20">
+                <div className="w-full">
+                    <h1 className="flex justify-center place-center my-20 opacity-75 my-0 ">About Us</h1>
                 </div>
+                <div className="w-full">
+                    <h3 className="flex text-center justify-center place-center my-20 opacity-75 text-3xl mx-36 ">
+                        A non-profit organization focused around informing people about our AI driven future.
+                    </h3>
+                </div>
+                <div>
+                    <p className="flex text-center justify-center place-center my-20 opacity-75 mx-96 my-0">
+                        At AiVolution, we are passionate about the infinite possibilities that technology offers and are dedicated to fostering a community that shares this enthusiasm. We are a non-profit organization, and our mission is clear: to be at the forefront of the ever-evolving world of technology by bringing people together through a series of exceptional events and conferences.
+                    </p>
+                </div>
+            </div>
+
+            {/* Section 3 */}
+            <div className="py-20">
+                <div className="w-full">
+                    <h1 className="flex justify-center place-center my-20 opacity-75 my-0">Who Are We</h1>
+                </div>
+                <Team />
             </div>
         </div>
     )
