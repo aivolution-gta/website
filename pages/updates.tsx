10dc75a6a3cbcb14bfe19438d4d384cce95fa034
advay-c <advay1612@icloud.com>
2023-12-31 20:52:11 -0500

    added instagram posts to updates pages + hyperlink

--- pages/updates.tsx
+++ pages/updates.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ShiftingCountdown from "@/components/countdown";
-
+import { CarouselDefault } from "@/components/carousel";
 
 export default function Updates() {
   return (
-    <div className="w-full">
+    <div className="w-full h-[2000px]">
       <h1 className="flex justify-center place-center my-5">Updates</h1>
       <h2 className="flex justify-center place-center">Days Till Next Summit</h2>
       <ShiftingCountdown /> 
-      
-    </div>
+      <h2 className="flex justify-center place-center my-[30px]">Latest Instagram Posts!</h2>
+      <CarouselDefault />
+      </div>
   );
 }
