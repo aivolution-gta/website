565cea7eaae13c56f5cd27838ae9fbc21b687b54
Aryan <vasudevan.aryan@gmail.com>
2024-06-28 00:30:07 -0400

    Fix team member section

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,74 +12,69 @@ import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 export default function Home() {
     const teamData = [
         {
-            role: "Members",
-            members: [
-                {
-                    name: "Ashwath",
-                    bio: "Hello, I'm Ashwath Jagadeesh, and I proudly serve as the President of AiVolution, steering the company through the dynamic realm of artificial intelligence with a visionary approach. Off-duty, I find my joy in the fast-paced worlds of badminton, volleyball, and basketball, relishing the spirit of teamwork and competition. I also harbor a deep passion for becoming an anesthesiologist.",
-                    links: { linkedin: "", gmail: "", instagram: "ashwath_j8" },
-                },
-                {
-                    name: "Devang",
-                    bio: "Hey, my name is Devang, and I am one of the presidents and founders of AiVolution. I am an inquisitive person who is always interested in learning more, which I believe is reflected in my hobbies and passions. In the future, I hope to become an astrophysicist and pursue a research career. Some of my hobbies are playing music, playing video games with my friends, and reading manhwa.",
-                    links: {
-                        linkedin: "https://www.linkedin.com/in/devang-kapahi",
-                        gmail: "",
-                        instagram: "_bronbo",
-                    },
-                },
-                {
-                    name: "Nirupa",
-                    bio: "Welcome to AiVolution’s website! My name is Nirupa and I am a vice-president at AiVolution. As VP, I help lead and manage the organization, constantly fostering growth and excellence. In the future, I plan on pursuing the medical field, specifically pharmaceutical chem., with hopes of becoming a pharmaceutical chemist. I am also passionate about volleyball, and enjoy playing competitively.",
-                    links: { linkedin: "", gmail: "", instagram: "" },
-                },
-                {
-                    name: "Taha",
-                    bio: "A passionate 'chef', an avid traveller, a soccer fan, and a  'tech-savvy guy' is what I'd call myself. I enjoy seeking knowledge and using technology to my advantage. Speaking of technology, I have countless hours to my name in photo and video editing; so I could call myself a Photoshop king. My latest products have been a satirical horror short film and a Q&A for Eid 2023.",
-                    links: {
-                        linkedin: "",
-                        gmail: "tahagh905@gmail.com",
-                        instagram: "talisman._.tg",
-                    },
-                },
-                {
-                    name: "Sohum",
-                    bio: "As the tech head of AiVolution, I have always been interested in creating and developing applications. Recently, I've gained a passion for web development, and creating websites is one of my favourite pastimes. I have been programming for over 5 years now, and I don't plan to give it up because of how rewarding it is. Aside from that, my interests include mathematics, playing volleyball, and playing chess.",
-                    links: {
-                        linkedin: "https://www.linkedin.com/in/sohum-padhye/",
-                        gmail: "sohum.padhye@gmail.com",
-                        instagram: "sohum.padhye",
-                    },
-                },
-                {
-                    name: "Aryan",
-                    bio: "Eager to embark on a journey of professional growth, I'm a high school student with a desire to gain knowledge in Programming and Computer Science. Through a combination of academic excellence, extracurricular involvement, and a thirst for knowledge, I am dedicated to improving my skills and gaining valuable experiences.",
-                    links: {
-                        linkedin: "https://www.linkedin.com/in/aryan-vasudevan",
-                        gmail: "vasudevan.aryan@gmail.com",
-                        instagram: "aryaan_v",
-                    },
-                },
-                {
-                    name: "Praney",
-                    bio: "Hi, my name is Praney. I'm a Grade 10 High School student and I'm the Head of Design at AiVolution. I enjoy most things art and technology related for as long as I remember. My goal at AiVolution is to help the organization and its own goals with what I can offer.",
-                    links: {
-                        linkedin: "",
-                        gmail: "praneyshivam@gmail.com",
-                        instagram: "praney0",
-                    },
-                },
-                {
-                    name: "Farhan",
-                    bio: "My name is Farhan Farhat and I am a executive of the marketing team for Aivolution. I'm a IB Grade 10 student attending Glenforest Secondary School. I'm an outgoing and extroverted person. I love learning more and more about the world around us and hope to become a neurosurgeon and persue a positive life. My hobbies including playing soccer, paying video games and cooking.",
-                    links: {
-                        linkedin:
-                            "https://www.linkedin.com/in/farhan-farhat-7490662b8/",
-                        gmail: "",
-                        instagram: "farhan_ferret",
-                    },
-                },
-            ],
+            name: "Ashwath",
+            bio: "Hello, I'm Ashwath Jagadeesh, and I proudly serve as the President of AiVolution, steering the company through the dynamic realm of artificial intelligence with a visionary approach. Off-duty, I find my joy in the fast-paced worlds of badminton, volleyball, and basketball, relishing the spirit of teamwork and competition. I also harbor a deep passion for becoming an anesthesiologist.",
+            links: { linkedin: "", gmail: "", instagram: "ashwath_j8" },
+        },
+        {
+            name: "Devang",
+            bio: "Hey, my name is Devang, and I am one of the presidents and founders of AiVolution. I am an inquisitive person who is always interested in learning more, which I believe is reflected in my hobbies and passions. In the future, I hope to become an astrophysicist and pursue a research career. Some of my hobbies are playing music, playing video games with my friends, and reading manhwa.",
+            links: {
+                linkedin: "https://www.linkedin.com/in/devang-kapahi",
+                gmail: "",
+                instagram: "_bronbo",
+            },
+        },
+        {
+            name: "Nirupa",
+            bio: "Welcome to AiVolution’s website! My name is Nirupa and I am a vice-president at AiVolution. As VP, I help lead and manage the organization, constantly fostering growth and excellence. In the future, I plan on pursuing the medical field, specifically pharmaceutical chem., with hopes of becoming a pharmaceutical chemist. I am also passionate about volleyball, and enjoy playing competitively.",
+            links: { linkedin: "", gmail: "", instagram: "" },
+        },
+        {
+            name: "Taha",
+            bio: "A passionate 'chef', an avid traveller, a soccer fan, and a  'tech-savvy guy' is what I'd call myself. I enjoy seeking knowledge and using technology to my advantage. Speaking of technology, I have countless hours to my name in photo and video editing; so I could call myself a Photoshop king. My latest products have been a satirical horror short film and a Q&A for Eid 2023.",
+            links: {
+                linkedin: "",
+                gmail: "tahagh905@gmail.com",
+                instagram: "talisman._.tg",
+            },
+        },
+        {
+            name: "Sohum",
+            bio: "As the tech head of AiVolution, I have always been interested in creating and developing applications. Recently, I've gained a passion for web development, and creating websites is one of my favourite pastimes. I have been programming for over 5 years now, and I don't plan to give it up because of how rewarding it is. Aside from that, my interests include mathematics, playing volleyball, and playing chess.",
+            links: {
+                linkedin: "https://www.linkedin.com/in/sohum-padhye/",
+                gmail: "sohum.padhye@gmail.com",
+                instagram: "sohum.padhye",
+            },
+        },
+        {
+            name: "Aryan",
+            bio: "Eager to embark on a journey of professional growth, I'm a high school student with a desire to gain knowledge in Programming and Computer Science. Through a combination of academic excellence, extracurricular involvement, and a thirst for knowledge, I am dedicated to improving my skills and gaining valuable experiences.",
+            links: {
+                linkedin: "https://www.linkedin.com/in/aryan-vasudevan",
+                gmail: "vasudevan.aryan@gmail.com",
+                instagram: "aryaan_v",
+            },
+        },
+        {
+            name: "Praney",
+            bio: "Hi, my name is Praney. I'm a Grade 10 High School student and I'm the Head of Design at AiVolution. I enjoy most things art and technology related for as long as I remember. My goal at AiVolution is to help the organization and its own goals with what I can offer.",
+            links: {
+                linkedin: "",
+                gmail: "praneyshivam@gmail.com",
+                instagram: "praney0",
+            },
+        },
+        {
+            name: "Farhan",
+            bio: "My name is Farhan Farhat and I am a executive of the marketing team for Aivolution. I'm a IB Grade 10 student attending Glenforest Secondary School. I'm an outgoing and extroverted person. I love learning more and more about the world around us and hope to become a neurosurgeon and persue a positive life. My hobbies including playing soccer, paying video games and cooking.",
+            links: {
+                linkedin:
+                    "https://www.linkedin.com/in/farhan-farhat-7490662b8/",
+                gmail: "",
+                instagram: "farhan_ferret",
+            },
         },
     ];
 
@@ -218,11 +213,10 @@ export default function Home() {
                     <Reveal>
                         <div className="grid gap-4 mx-10 sm:hidden xs:hidden">
                             <div className="">
-                                {teamData.map((data, index) => (
                                     <div
                                         className={`grid grid-cols-2 text-center gap-4`}
                                     >
-                                        {data.members.map((person, i) => (
+                                        {teamData.map((person, i) => (
                                             <div
                                                 className="col-span-1 border-2 border-dark-purple rounded-md select-none transition-all ease-in-out hover:bg-dark-purple duration-300 hover:cursor-pointer hover:scale-105"
                                                 key={i}
@@ -235,136 +229,11 @@ export default function Home() {
                                             </div>
                                         ))}
                                     </div>
-                                ))}
-                            </div>
-                            <div className="col-span-1">
-                                {teamData.map((data, index) =>
-                                    index % 2 === 1 ? (
-                                        <Accordion
-                                            key={index}
-                                            title={data.role}
-                                            description={
-                                                <div
-                                                    className={`grid grid-cols-2 text-center gap-4`}
-                                                >
-                                                    {data.members.map(
-                                                        (person, i) => (
-                                                            <div
-                                                                className="col-span-1 border-2 border-dark-purple rounded-md select-none transition-all ease-in-out hover:bg-dark-purple duration-300 hover:cursor-pointer hover:scale-105"
-                                                                key={i}
-                                                            >
-                                                                <MuiModal
-                                                                    name={
-                                                                        person.name
-                                                                    }
-                                                                    bio={
-                                                                        person.bio
-                                                                    }
-                                                                    links={
-                                                                        person.links
-                                                                    }
-                                                                />
-                                                            </div>
-                                                        )
-                                                    )}
-                                                </div>
-                                            }
-                                        />
-                                    ) : (
-                                        ""
-                                    )
-                                )}
-                            </div>
-                        </div>
-                    </Reveal>
-
-                    <Reveal>
-                        <div className="grid grid-cols-1 gap-4 mx-4 hidden sm:block xs:block">
-                            <div className="col-span-1">
-                                {teamData.map((data, index) => (
-                                    <Accordion
-                                        key={index}
-                                        title={data.role}
-                                        description={
-                                            <div
-                                                className={`grid grid-cols-1 text-center gap-4`}
-                                            >
-                                                {data.members.map(
-                                                    (person, i) => (
-                                                        <div
-                                                            className="col-span-1 border-2 border-dark-purple rounded-md py-4 select-none transition-all ease-in-out hover:bg-dark-purple duration-300 hover:cursor-pointer hover:scale-105"
-                                                            key={i}
-                                                        >
-                                                            <MuiModal
-                                                                name={
-                                                                    person.name
-                                                                }
-                                                                bio={person.bio}
-                                                                links={
-                                                                    person.links
-                                                                }
-                                                            />
-                                                        </div>
-                                                    )
-                                                )}
-                                            </div>
-                                        }
-                                    />
-                                ))}
                             </div>
                         </div>
                     </Reveal>
                 </div>
             </div>
-
-            {/* Partners and Sponsors */}
-            {/* <div className="w-full flex flex-col place-items-center my-16">
-                <Reveal><h1 className="xs:text-[2.5em]">Partners</h1></Reveal>
-                <div className="w-5/6 rounded-lg grid grid-cols-1 gap-8 p-8 place-items-center sm:hidden xs:hidden">
-                    
-                    {partners.map((partner, i) => (
-                        <Reveal key={i}>
-                        <div className="">
-                            {
-                                i % 2 == 0 ? 
-                                <div className="rounded-md p-2 grid grid-cols-3 w-full m-4">
-                                    <div className="w-full flex justify-center">
-                                        <a href={partner.link} target="_blank"><Image src={partner.img} alt="partner" className=""/></a>
-                                    </div>
-                                    <div className="col-span-2 flex flex-col justify-evenly">
-                                        <h2 className="text-center ">{partner.name}</h2>
-                                        <p className="text-justify leading-loose xs:text-[1em]">{partner.bio}</p>
-                                    </div>
-                                </div>
-                                :
-                                <div className="rounded-md p-2 grid grid-cols-3 w-full m-4 ">
-                                    <div className="col-span-2 flex flex-col justify-evenly">
-                                        <h2 className="text-center">{partner.name}</h2>
-                                        <p className="text-justify leading-loose xs:text-[1em]">{partner.bio}</p>
-                                    </div>
-                                    <div className="w-full flex justify-center">
-                                        <a href={partner.link} target="_blank"><Image src={partner.img} alt="partner" className=""/></a>
-                                    </div>
-                                </div>
-                            }
-                        </div>
-                        </Reveal>
-                    ))}
-                </div>
-
-                <div className="hidden xs:w-7/8 sm:w-7/8 xs:rounded-lg sm:rounded-lg xs:grid sm:grid xs:grid-cols-1 sm:grid-cols-1 xs:gap-8 sm:gap-8 xs:p-8 sm:p-8 xs:place-items-center sm:place-items-center">
-                    
-                    {partners.map((partner, i) => (
-                        <Reveal key={i}>
-                        <div className="w-full flex flex-col place-items-center justify-evenly">
-                            <a href={partner.link} target="_blank"><Image src={partner.img} alt="partner" className=""/></a>
-                            <h2 className="text-center text-[1.75em]">{partner.name}</h2>
-                            <p className="text-justify leading-loose">{partner.bio}</p>
-                            <hr />
-                        </div>
-                        </Reveal>
-                    ))}
-                </div> */}
 
             {/* <h1>Sponsors</h1>
                 <div className=""></div> */}
